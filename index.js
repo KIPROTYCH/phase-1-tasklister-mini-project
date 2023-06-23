0bba26211ea085959d91640cbbf14673cec7b635
KIPROTYCH <kiprotych@gmail.com>
2023-06-23 11:15:42 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("create-task-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    
+  });
